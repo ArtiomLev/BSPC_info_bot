53cb8e4e76e80a4ce434e3ed9788612e553eb50d
Artiom Levshukov <artiomlevshykov@gmail.com>
2025-04-22 14:00:39 +0300

    feat: update escape function for telegram

--- my_escape_function.py
+++ my_escape_function.py
@@ -3,4 +3,4 @@ import re
 
 def escape_for_telegram(text: str) -> str:
     """Экранирует символы '(', ')', '-' в строке."""
-    return re.sub(r'([()\-/!\[\]|=])', r'\\\1', text)
+    return re.sub(r'([()/!\[\]|=.])', r'\\\1', text)
